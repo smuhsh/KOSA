71b0067e538016df1dd631e72f0e007af908a70a
smuhsh <user@DESKTOP-C5EEQRQ>
2024-06-27 15:37:21 +0900

    2024-06-27 BufferedInputOutputStream update

--- javadev/src/sec04_filterinputoutputstream/EX01_BufferedInputOutputStream/BufferedInputOutputStream.java
+++ javadev/src/sec04_filterinputoutputstream/EX01_BufferedInputOutputStream/BufferedInputOutputStream.java
@@ -14,40 +14,58 @@ import java.io.OutputStream;
 /*
  * 작성일:2024-06-27
  * 작성자:황석현
- * 개요: 교재p821 ~ 822
- * 		PirntStream 객체 생성 및 활용(File 출력, OutputStream 출력, 콘솔출력)
+ * 개요: 교재p812 ~ 814
+ * 		Buffered(Input/Output)Stream 클래스를 활용한 입출력 속도 개선
  * 
  */
 public class BufferedInputOutputStream {
 
-	public static void main(String[] args) throws IOException {
-		
-		//파일 생성
-		File dataFile = new File("src/sec04_filterinputoutputstream/files/file2.data");
-		
-		//데이터 쓰기 (DataOutputStream)
-		try(OutputStream os = new FileOutputStream(dataFile);
-			BufferedOutputStream bos = new BufferedOutputStream(os);
-			DataOutputStream dos = new DataOutputStream(bos);){
-			
-			dos.writeInt(35);
-			dos.writeDouble(5.8);
-			dos.writeChar('A');
-			dos.writeUTF("안녕하세요");
-			dos.flush();			
-		}		
-		
-		//데이터 읽기 (DataInputStream)
-		try(InputStream is = new FileInputStream(dataFile);
-			BufferedInputStream bis = new BufferedInputStream(is);
-			DataInputStream dis = new DataInputStream(bis);){
-			
-			System.out.println(dis.readInt());
-			System.out.println(dis.readDouble());
-			System.out.println(dis.readChar());
-			System.out.println(dis.readUTF());
-	
-		}	
-	}
+	public static void main(String[] args) {
 
+		//#파일 생성
+		File orgfile = new File("src\\sec04_filterinputoutputstream\\EX01_BufferedInputOutputStream\\files\\pome.webp");
+		File copyfile1 = new File("src\\sec04_filterinputoutputstream\\EX01_BufferedInputOutputStream\\files\\pome1.webp");
+		File copyfile2 = new File("src\\sec04_filterinputoutputstream\\EX01_BufferedInputOutputStream\\files\\pome2.webp");
+
+		long start, end, time1, time2;
+		//#1. BufferedInputStream, BufferedOutputStream을 사용하지 않는 경우
+
+		start = System.nanoTime();
+		try (InputStream is = new FileInputStream(orgfile);
+			OutputStream os = new FileOutputStream(copyfile1);) {
+
+				//복사하는 코드
+				int data;
+				while((data=is.read())!=-1) {
+					os.write(data);
+				}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		end = System.nanoTime();
+		time1 = end - start;
+		System.out.println("Without BufferedXXXStream :"+time1);
+
+		//#2. BufferedInputStream, BufferedOutputStream을 사용한 경우
+		start = System.nanoTime();
+		try (InputStream is = new FileInputStream(orgfile);
+				BufferedInputStream bis = new BufferedInputStream(is);
+				OutputStream os = new FileOutputStream(copyfile2);
+				BufferedOutputStream bos = new BufferedOutputStream(os);){
+					//복사하는 코드
+					int data;
+					while((data = bis.read())!=-1) {
+						bos.write(data);
+					}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		end = System.nanoTime();
+		time2 = end = start;
+		System.out.println("With BufferedXXXStream : " +time2);
+
+		//사용한 경우와 사용하지 않은 경우 비교
+		System.out.println("Ratio of with and without : " +time1/time2);
+	}
 }
